dc064aa6c9c323c17cbeb3c792a7146ed709629c
Ankitmaurya-1 <ankitmaurya9560@gmail.com>
2024-08-22 17:32:54 +0530

    Added Love to footer

--- src/app/page.js
+++ src/app/page.js
@@ -74,7 +74,7 @@ const LandingPage = () => {
 
       <footer className="bg-gray-800 text-white py-6">
         <div className="container mx-auto text-center">
-          <p>&copy; 2024 Developer Ankit. All rights reserved.</p>
+          <p> Made with <span role="img" aria-label="love">❤️</span>Developer Ankit.</p>
         </div>
       </footer>
     </div>
